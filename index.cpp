95af24a33657dec1db3ddbe615c9af221df58dce
Jonathan Samuels <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-05-14 10:28:32 -0700

    Add swap and selection sort algorithms

--- index.cpp
+++ index.cpp
@@ -243,13 +243,46 @@ public:
 
 
 
-    void vectorBubbleSort(){
-        for(int i = 0; i < size; i++){
-            if (arr[i + 1] > arr[1] ){
-                arr[i] = arr[i + 1]; 
-            }
-        }
-    }
+    // void swap_(int arr[], int n) {
+    //     bool swapped;
+    //     for (int i = 0; i < n - 1; i++) {
+    //         swapped = false;
+    //         for (int j = 0; j < n - i - 1; j++) {
+    //             if (arr[j] > arr[j + 1]) {
+    //                 int c = arr[j];
+    //                 arr[j] = arr[j + 1];
+    //                 arr[j + 1] = c;
+    //                 swapped = true;
+    //             }
+    //         }
+    //         if (!swapped) {
+    //             break;
+    //         } 
+    //         cout << endl;
+    //         cout << "Sorting pass " << i+1 << ": "; 
+    //         for (int l = 0; l < n; l++){
+    //             cout << arr[l] << " ";
+    //         }
+    //         cout << "" << endl;
+    //     }
+
+    // }
+
+    // void selectSort(int arr[], int n) {
+    //     for (int i = 0; i < n - 1; i++) {
+    //         int minIndex = i;
+    //         for (int j = i + 1; j < n; j++) {
+    //             if (arr[j] < arr[minIndex]) {
+    //                 minIndex = j;
+    //             }
+    //         }
+    //         int temp = arr[i];
+    //         arr[i] = arr[minIndex];
+    //         arr[minIndex] = temp;
+            
+    //     }
+    // }
+
 
     void listt(){
         std::cout << "pushBack() To add to the back of the array. " << std::endl;
